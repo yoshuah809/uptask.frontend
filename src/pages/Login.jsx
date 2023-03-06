092223276f48d8aaa2af1e16240a259e770d8e4e
JOsue Garcia <yoshuah809@gmail.com>
2023-03-06 06:47:44 -0600

    finished loging form presentation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,36 +1,57 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <>
-      <span className="display-1 text-info ls-0 fw-bold text-capitalize">
+    <div className="shadow rounded d-flex flex-column align-items-center justify-content-center">
+      <span className="display-1 text-info ls-0 fw-bold text-capitalize mb-2 mt-5">
         Login to view and work your <span className="text-muted">projects</span>
       </span>
-      <form className="shadow rounded p-5">
+      <form className="w-50">
         <div className="form-group">
-          <div className="form-floating mt-4">
+          <div className="form-floating my-5">
             <input
               type="email"
               className="form-control rounded"
               id="floatingInput"
               placeholder="name@example.com"
             />
-            <label for="floatingInput">Email</label>
+            <label className="text-uppercase text-info" for="floatingInput">
+              Email
+            </label>
           </div>
         </div>
         <div className="form-group">
-          <div className="form-floating mt-4">
+          <div className="form-floating my-5">
             <input
               type="password"
               className="form-control rounded"
               id="floatingInput"
               placeholder="name@example.com"
             />
-            <label for="floatingInput">Password</label>
+            <label className="text-uppercase text-info" for="floatingInput">
+              Password
+            </label>
+          </div>
+          <div>
+            <button type="submit " className="btn btn-outline-info mb-5 w-100">
+              Sing in
+            </button>
           </div>
         </div>
       </form>
-    </>
+      <nav className="d-lg-flex justify-content-between">
+        <Link className="d-block text-center my-5 m-5 text-muted" to="register">
+          {" "}
+          Don't Have An account? Register
+        </Link>
+        <Link
+          className="d-block text-center my-5 text-warning"
+          to="forgot-password"
+        >
+          Forgot Password?
+        </Link>
+      </nav>
+    </div>
   );
 };
 
